b2ab851bd0b66bb60d0084ff86204edadf9131eb
Henrik Palmlund Wahlgren <henrik@pwit.se>
2018-02-24 15:46:05 +0100

    moved over files from dlms_cosem and added setup file

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='amr-crypto',
+    version='0.0.1',
+    description='Cryptography for Automatic Meter Reading Systems.',
+    url='https://github.com/pwitab/amr-crypto',
+    author='Henrik Palmlund Wahlgren, '
+           'Palmlund Wahlgren Innovative Technology AB',
+    author_email='henrik@pwit.se',
+    license='BSD 3-Clause License',
+    packages=['amr_crypto'],
+    install_requires=['cryptography', ]
+
+)
